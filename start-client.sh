a4f1a73c3e72117efe4e22458b1ee88f9341384d
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-12 16:15:14 +0200

    Update start-client.sh

--- start-client.sh
+++ start-client.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 
+echo "Most used commands:"
+echo "aethir licenses list --pending"
+echo "aethir licenses list --all"
+echo "aethir license approve -all"
+echo "aethir wallet create"
+
 cd ~/aethir
 ./AethirCheckerCLI
